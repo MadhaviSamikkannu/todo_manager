7ead4e8f2fbb03ad6e3b396ed593196350b21112
MadhaviSamikkannu <madhavis@ksrct.ac.in>
2021-10-03 19:58:34 +0530

    Patch is learnt

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -25,4 +25,13 @@ class TodosController < ApplicationController
         response_text = "Hey i am creating new todo eith the id #{new_todo.id}"
        render plain: response_text
   end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save
+    render plain: " Updated todo completed status to #{completed}"
+  end
 end
