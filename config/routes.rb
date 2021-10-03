d0b00ac2aa1812a894d7aa105e5e958c61fcad2c
MadhaviSamikkannu <madhavis@ksrct.ac.in>
2021-10-03 19:30:18 +0530

    Resources routing convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "todos", to: "todos#index"
-  post "todos/create", to: "todos#create"
-  get "todos/:id", to: "todos#show"
-  end
+  #get "todos", to: "todos#index"
+  #post "todos", to: "todos#create"
+  #get "todos/:id", to: "todos#show"
+  resources :todos
+end
